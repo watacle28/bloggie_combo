a2792eb22c96cd030578c45494b894fd76f37bcc
watacle <sirwatacle@gmail.com>
2020-09-07 15:51:17 +0200

    heroku commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
